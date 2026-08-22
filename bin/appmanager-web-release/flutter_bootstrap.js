2dc55d3d46c76df3d9ed8508c0774dc97d90630f
everyonelovespepsicola <40620033+everyonelovespepsicola@users.noreply.github.com>
2026-08-22 06:39:17 -0700

    feat(assets): update official app logo icon to flat superhero Android green shield emblem
    - Embedded flat superhero Android green shield logo icon (app_icon_flat_superhero.ico) into Windows desktop executable (windows/runner/resources/app_icon.ico & bin/appmanager-windows-release/app_manager.exe)
    - Updated web favicon assets (web/favicon.png & bin/appmanager-web-release/favicon.png)
    - Added new logo icon source assets into project assets folder

--- bin/appmanager-web-release/flutter_bootstrap.js
+++ bin/appmanager-web-release/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "728405325" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1159712618" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
